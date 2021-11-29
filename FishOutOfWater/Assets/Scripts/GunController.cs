8212828cb4b84126ad571a8f961239b7b14e092b
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-11-29 15:27:29 +0100

    ammo added

--- FishOutOfWater/Assets/Scripts/GunController.cs
+++ FishOutOfWater/Assets/Scripts/GunController.cs
@@ -11,61 +11,88 @@ public class GunController : MonoBehaviour
     private float timeBtwShots, startTimeBtwShots;
     private PlayerHealth playerHealth;
 
+    private int ammo;
+
     private void Start()
     {
-        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        ammo = 6;
         startTimeBtwShots = 0.2f;
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     private void Update()
     {
-            if (timeBtwShots <= 0 && playerHealth.currentHealth > 0)
-            {
+        if (timeBtwShots <= 0 && playerHealth.currentHealth > 0)
+        {
 
-                if (Input.GetKeyDown(KeyCode.LeftArrow))
-                {
-                    playerController.SetPlayerRotation(1, 0);
-                    Fire(1, 0);
-                    playerController.SwitchGravity(0f);
-                }
-                else if (Input.GetKeyDown(KeyCode.RightArrow))
-                {
-                    playerController.SetPlayerRotation(-1, 0);
-                    Fire(-1, 0);
-                    playerController.SwitchGravity(0f);
-                }
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    playerController.SetPlayerRotation(0, 1);
-                    Fire(0, 1);
-                    playerController.SwitchGravity(8.92f);
-                }
-                else if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    playerController.SetPlayerRotation(0, -1);
-                    Fire(0, -1);
-                    playerController.SwitchGravity(8.92f);
-                }
-            }
-            else
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                timeBtwShots -= Time.deltaTime;
+                playerController.SetPlayerRotation(1, 0);
+                Fire(1, 0);
+                playerController.SwitchGravity(0f);
             }
-
-            if (playerController.rb.velocity.x < 4f && playerController.rb.velocity.x > -4f)
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
+                playerController.SetPlayerRotation(-1, 0);
+                Fire(-1, 0);
+                playerController.SwitchGravity(0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                playerController.SetPlayerRotation(0, 1);
+                Fire(0, 1);
                 playerController.SwitchGravity(8.92f);
             }
-        
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                playerController.SetPlayerRotation(0, -1);
+                Fire(0, -1);
+                playerController.SwitchGravity(8.92f);
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+        if (playerController.rb.velocity.x < 4f && playerController.rb.velocity.x > -4f)
+        {
+            playerController.SwitchGravity(8.92f);
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)  && ammo <= 0)
+        {
+            Invoke("Reload", 1.0f);
+        }
     }
 
     public void Fire(int directionX, int directionY)
     {
-        GameObject bullet = Instantiate(projectile, shotPoint.position, transform.rotation);
-        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(directionX, directionY) * 20f;
-        playerController.Movement(directionX, directionY);
-        timeBtwShots = startTimeBtwShots;
-        Destroy(bullet, 3);
+        ammo--;
+        if(ammo > 0)
+        {
+            Debug.Log("Ammo: " + ammo);
+            GameObject bullet = Instantiate(projectile, shotPoint.position, transform.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(directionX, directionY) * 20f;
+            playerController.Movement(directionX, directionY);
+            timeBtwShots = startTimeBtwShots;
+            Destroy(bullet, 3);
+        }
+
+        if(ammo <= 0)
+        {
+            Debug.Log("Out of ammo, press Space to reload");
+        }
+    }
+
+    private void Reload()
+    {
+        ammo = 6;
+        Debug.Log("Chamber filled!");
     }
 }
