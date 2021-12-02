453cc228398336a81772184277e14f8309cc1f3e
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-02 12:17:44 +0100

    invunerability added

--- FishOutOfWater/Assets/Scripts/ToxicBarrel.cs
+++ FishOutOfWater/Assets/Scripts/ToxicBarrel.cs
@@ -6,18 +6,32 @@ public class ToxicBarrel : MonoBehaviour
 {
     public ParticleSystem toxicExplosion;
     private PlayerHealth playerHealth;
+    public bool invincible;
+
+    private void Start()
+    {
+        invincible = false;
+    }
 
     void OnParticleCollision(GameObject other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !invincible)
         {
             playerHealth = other.GetComponent<PlayerHealth>();
             playerHealth.TakeDamage(20);
+            invincible = true;
+
+            Invoke("resetInvulnerability", 1);
         }
     }
 
     public void CreateToxicExplosion()
     {
         toxicExplosion.Play();
+    }
+
+    private void resetInvulnerability()
+    {
+        invincible = false;
     }
 }
