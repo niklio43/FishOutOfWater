69b61abbfaa9e688a1205da40d6b8e0028f33b55
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-04 18:23:55 +0100

    added scriptable objects for guns

--- FishOutOfWater/Assets/Objects/GunObj.cs
+++ FishOutOfWater/Assets/Objects/GunObj.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunObj : MonoBehaviour
+[CreateAssetMenu(fileName = "Weapon", menuName = "Objects/Weapon", order = 0)]
+public class GunObj : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float ammo;
+    public GameObject projectile;
+    public float timeBtwShots;
+    public float thrust;
+    public float amoutOfBullet;
+    public float spread;
 }
