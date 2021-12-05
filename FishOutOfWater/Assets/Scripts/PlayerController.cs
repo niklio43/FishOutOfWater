3998b1616ba56d406e416185771a6a4dccb14421
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-05 18:30:15 +0100

    added slower movement when on ground for precise positioning

--- FishOutOfWater/Assets/Scripts/PlayerController.cs
+++ FishOutOfWater/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float thrust, jumpForce;
+    private float thrust, jumpForce;
     public Rigidbody2D rb;
     public ParticleSystem dust;
 
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         isGrounded = false;
+        jumpForce = 30;
         rb = GetComponent<Rigidbody2D>();
         playerHealth = GetComponent<PlayerHealth>();
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -28,6 +29,15 @@ public class PlayerController : MonoBehaviour
     {
         if(GameObject.FindGameObjectWithTag("FishNet") != null)
             fishNetController = GameObject.FindGameObjectWithTag("FishNet").GetComponent<FishNetController>();
+
+        if (isGrounded)
+        {
+            thrust = 15;
+        }
+        else
+        {
+            thrust = 20;
+        }
     }
 
     public void Movement(int directionX, int directionY)
