04564b72816ff15a0c3851cbcefa0fc7993097d9
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-06 11:29:15 +0100

    win screen added

--- FishOutOfWater/Assets/Scripts/MainMenu.cs
+++ FishOutOfWater/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
