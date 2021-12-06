b46d23b02897aba3a1fccee0a880ea9eda694345
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-06 14:54:22 +0100

    new ammo counter added

---
+++ FishOutOfWater/Assets/Scripts/DisplayAmmo.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayAmmo : MonoBehaviour
+{
+    public GameObject enemyPefab;
+    private GunController gunController;
+    private float radius, radians, vertical, horizontal;
+    private Vector2 point, spawnDir, spawnPos;
+    private int numOfAmmo;
+    private GameObject ammoCounter;
+    private bool finished;
+
+    private void Start()
+    {
+        finished = false;
+        gunController = gameObject.transform.GetChild(0).GetComponent<GunController>();
+        point = transform.position;
+        radius = 2;
+    }
+
+    private void Update()
+    {
+        numOfAmmo = gunController.ammo;
+        if (!finished)
+        {
+            for (int i = 0; i <= numOfAmmo; i++)
+            {
+                radians = Mathf.PI / numOfAmmo * i;
+
+                vertical = Mathf.Sin(radians);
+                horizontal = Mathf.Cos(radians);
+
+                spawnDir = new Vector2(horizontal, vertical);
+
+                spawnPos = point + spawnDir * radius;
+
+                if(i == numOfAmmo)
+                {
+                    finished = true;
+                }
+
+                CreateEnemiesAroundPoint();
+            }
+        }
+    }
+
+    public void CreateEnemiesAroundPoint()
+    {
+            ammoCounter = Instantiate(enemyPefab, spawnPos, transform.rotation, gameObject.transform) as GameObject;
+    }
+
+    private void removeAmmo()
+    {
+        Destroy(gameObject.transform.GetChild(2).gameObject);
+    }
+}
