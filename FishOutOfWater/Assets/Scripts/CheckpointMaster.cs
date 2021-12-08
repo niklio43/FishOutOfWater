778d2e71a9ff396fa16cdd3151db6ff36c6399bf
WashFall <carl.christensen98@gmail.com>
2021-12-08 13:50:11 +0100

    Added checkpoints

---
+++ FishOutOfWater/Assets/Scripts/CheckpointMaster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointMaster : MonoBehaviour
+{
+    public Vector2 lastCPPos;
+
+    private static CheckpointMaster instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
