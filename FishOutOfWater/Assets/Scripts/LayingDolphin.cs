71a69b9f0ebac52adcafc8ccad16cf539dd26652
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-09 09:43:35 +0100

    layingdolphin animations

--- FishOutOfWater/Assets/Scripts/LayingDolphin.cs
+++ FishOutOfWater/Assets/Scripts/LayingDolphin.cs
@@ -7,25 +7,39 @@ public class LayingDolphin : MonoBehaviour
     private PlayerHealth playerHealth;
     private Vector2 targetPos;
 
+    public Animator anim;
+
+    private bool isMoving;
+
     void Start()
     {
+        isMoving = false;
         Health = 60;
         Player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = Player.GetComponent<PlayerHealth>();
+    }
+
+    private void Update()
+    {
+        if(isMoving)
+            anim.SetTrigger("isMoving");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && Health > 0)
         {
+            anim.SetTrigger("isAttacking");
             playerHealth.TakeDamage(20);
+            isMoving = false;
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && Health > 0)
         {
+            isMoving = true;
             targetPos = Player.transform.position;
             transform.position = Vector3.MoveTowards(transform.position, targetPos, 15 * Time.deltaTime);
         }
