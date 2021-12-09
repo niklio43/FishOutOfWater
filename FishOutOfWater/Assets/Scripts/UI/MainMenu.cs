a5be3faa3e29b44e7cd6392491f71f100a206d53
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-09 18:06:40 +0100

    fixed so game can be quit and moved player in prep for playtest

--- FishOutOfWater/Assets/Scripts/UI/MainMenu.cs
+++ FishOutOfWater/Assets/Scripts/UI/MainMenu.cs
@@ -15,6 +15,12 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+    #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+             Application.Quit();
+    #endif
     }
 }
--- FishOutOfWater/Assets/Scripts/UI/PauseMenu.cs
+++ FishOutOfWater/Assets/Scripts/UI/PauseMenu.cs
@@ -45,7 +45,12 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("Quit game");
-        Application.Quit();
+    #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+                 Application.Quit();
+    #endif
     }
 }
