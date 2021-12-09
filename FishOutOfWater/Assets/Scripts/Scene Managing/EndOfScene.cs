3f9784016584e40fdafa1a54a93073c9b4b7da8d
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-09 18:02:50 +0100

    fixed checkpoint error when no checkpoints exists

--- FishOutOfWater/Assets/Scripts/Scene Managing/EndOfScene.cs
+++ FishOutOfWater/Assets/Scripts/Scene Managing/EndOfScene.cs
@@ -7,14 +7,16 @@ public class EndOfScene : MonoBehaviour
 
     private void Start()
     {
-        cpm = GameObject.FindGameObjectWithTag("CPM").GetComponent<CheckpointMaster>();
+        if(GameObject.FindGameObjectWithTag("CPM") != null)
+            cpm = GameObject.FindGameObjectWithTag("CPM").GetComponent<CheckpointMaster>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            cpm.lastCPPos = new Vector2(-7.183609f, -4.734975f);
+            if(cpm != null)
+                cpm.lastCPPos = new Vector2(-7.183609f, -4.734975f);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
