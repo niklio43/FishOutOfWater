701c8674903ebc65046ee1a39a9a662814efc30f
WashFall <carl.christensen98@gmail.com>
2021-12-21 11:35:49 +0100

    Oopsie camera

--- FishOutOfWater/Assets/Scripts/Camera/CameraFollow.cs
+++ FishOutOfWater/Assets/Scripts/Camera/CameraFollow.cs
@@ -12,7 +12,11 @@ public class CameraFollow : MonoBehaviour
     {
         if (target)
         {
-            Vector3 delta = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, transform.position.z));
+            Vector3 aheadPoint = target.position +
+                new Vector3(target.GetComponent<Rigidbody2D>().velocity.x * 0.8f, target.GetComponent<Rigidbody2D>().velocity.y * 0.5f, 0);
+
+            Vector3 point = Camera.main.WorldToViewportPoint(aheadPoint);
+            Vector3 delta = aheadPoint - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, smoothSpeed);
         }
