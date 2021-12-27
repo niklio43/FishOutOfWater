c1ff647cc04328e63e143163cb57ec4bb43c8ba2
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-27 14:47:00 +0100

    new camera

---
+++ FishOutOfWater/Assets/Scripts/Room.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    public GameObject virtualCam;
+
+    private void Awake()
+    {
+        virtualCam.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
