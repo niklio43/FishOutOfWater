6c74c401663d90956f5a38e1bd049eecba60b6a9
WashFall <carl.christensen98@gmail.com>
2021-12-30 14:50:48 +0100

    Updated inputs

--- FishOutOfWater/Assets/Scripts/Scene Managing/PlaytestRestart.cs
+++ FishOutOfWater/Assets/Scripts/Scene Managing/PlaytestRestart.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class PlaytestRestart : MonoBehaviour
 {
     public string sceneName;
 
+    private Inputs inputs;
+
     void Start()
     {
         sceneName = SceneManager.GetActiveScene().name;
+        inputs = new Inputs();
+        inputs.Enable();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (inputs.Player.Restart.triggered)
         {
             SceneManager.LoadScene(sceneName);
         }
