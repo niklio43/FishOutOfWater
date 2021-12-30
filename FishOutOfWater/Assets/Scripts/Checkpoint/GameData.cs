f1d09b75c6103e41b82e27d37828d5101339fbf5
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-12-30 13:54:51 +0100

    removed deathcounter temporarily

--- FishOutOfWater/Assets/Scripts/Checkpoint/GameData.cs
+++ FishOutOfWater/Assets/Scripts/Checkpoint/GameData.cs
@@ -3,12 +3,12 @@ public class GameData
 {
     public int scene;
     public float timePlayed;
-    public int deathCounter;
+    //public int deathCounter;
 
     public GameData(Checkpoint checkpoint)
     {
         scene = checkpoint.scene;
         timePlayed = checkpoint.timePlayed;
-        deathCounter = checkpoint.deathCounter;
+        //deathCounter = checkpoint.deathCounter;
     }
 }
